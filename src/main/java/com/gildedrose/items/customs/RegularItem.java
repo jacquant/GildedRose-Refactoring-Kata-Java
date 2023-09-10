d5f19ddb60495074fa7f596f3e468b7ebb67fb93
Antoine Jacques <antoine.jacques@outlook.be>
2023-09-10 20:03:29 +0200

    refactor: introduce constant to support conjured items

--- src/main/java/com/gildedrose/items/customs/RegularItem.java
+++ src/main/java/com/gildedrose/items/customs/RegularItem.java
@@ -5,11 +5,11 @@ import com.gildedrose.items.Item;
 import com.gildedrose.items.rules.QualityRule;
 import com.gildedrose.items.rules.SellInDirection;
 import com.gildedrose.items.rules.SellInRule;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@SellInRule(by = 1)
-@QualityRule(by = -1, whenSellInIs = 0, sellInDirection = SellInDirection.OVER, order = 0)
-@QualityRule(by = -2, whenSellInIs = 0, sellInDirection = SellInDirection.UNDER_AND_EQUALS, order = 1)
+@SellInRule(by = RegularItem.SELL_IN_VALUE)
+@QualityRule(by = RegularItem.DECREASE_VALUE, whenSellInIs = 0, sellInDirection = SellInDirection.OVER, order = 0)
+@QualityRule(by = RegularItem.DECREASE_VALUE *2 , whenSellInIs = 0, sellInDirection = SellInDirection.UNDER_AND_EQUALS, order = 1)
 public record RegularItem(Item item) implements GildedRoseItem {
+    public static final int DECREASE_VALUE = -1;
+    public static final int SELL_IN_VALUE = 1;
 }
